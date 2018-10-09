4681c826c4f21067d668be43ce564b4522f30432
Juan López Rubio <j.lopez.r@gmail.com>
2018-10-09 12:51:57 +0200

    Added a / route

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 https://www.joshmorony.com/adding-es6-typescript-support-to-a-nodejs-server/
+
+```
+npm i -g typescript
+tsc
+node build/server.js
+```
--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import * as bodyParser from 'body-parser';
 import * as logger from 'morgan';
 import * as methodOverride from 'method-override';
 import * as cors from 'cors';
+import {Request, Response} from 'express';
 
 const app = express();
 app.use(logger('dev'));
@@ -12,4 +13,9 @@ app.use(cors());
 
 let bigRedDog = 'Clifford';
 
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, '+bigRedDog);
+});
+
 app.listen(process.env.PORT || 8080);
+console.log("Server started on port 8080");
