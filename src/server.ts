5b3ce9b8138b69d483596fa14869e8d88478338b
Juan López Rubio <j.lopez.r@gmail.com>
2020-05-01 16:50:35 +0200

    tslint

--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,20 @@ import * as methodOverride from 'method-override';
 import * as cors from 'cors';
 import {Request, Response} from 'express';
 
+function x(a,b) {
+    return a+b;
+}
+
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(methodOverride());
 app.use(cors());
 
-let bigRedDog = 'Clifford';
+const bigRedDog = 'Clifford';
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, '+bigRedDog);
+    res.send('Hello, '+bigRedDog+x(1,2));
 });
 
 app.listen(process.env.PORT || 8080);
